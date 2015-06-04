6a69ad368d850ae2db6d401f3f3577d926a9dc71
Dirk <stahdirk@mit.edu>
2015-06-03 21:36:57 -0400

    added printing options (basic) and name guesser

--- main.py
+++ main.py
@@ -1,9 +1,26 @@
 import argparse
 import re
+import operator
 
-namesList = [];
+namesList = []
 wordsDict = {}
 namesDict = {}
+guessedNamesSet = set()
+
+#try to guess what is a name by looking for capitalized letters in the middle of sentences
+def getGuessedNames():
+    pass
+
+def guessNames(line):
+    nameRegex = re.compile('[^\.] ([ABCDEFGHIJKLMNOPQRSTUVWXYZ]\w+)')
+    names = nameRegex.search(line)
+
+    try: 
+        for name in names.groups():
+            guessedNamesSet.add(name)
+        print names.groups() #TODO: regex is broken (doesn't capture all matches)
+    except:
+        return
 
 #parse a line and add the words to the dictionaries
 def addLine(line, forNames):
@@ -24,22 +41,38 @@ def addLine(line, forNames):
             else:
                 wordsDict[word] = 1
 
-    print wordsDict
+
+def printHighest(num):
+    sortedWordsDict = sorted(wordsDict.items(), key=operator.itemgetter(1))
+    sortedWordsDict.reverse()
+    for x in xrange(0,num):
+        print sortedWordsDict[x];
 
 def main(url, forNames=False):
     f = open(url, 'r')
     line = f.readline()
+    while (line != ''):
 
-    #check a line to see if it's a date, therefore a new day
-    newdate = re.compile('\s*[0-9]{1,2}-[0-9]{1,2}-[0-9]{1,2}\s*')
-    res = newdate.match(line)
-    if res != None: #date found
-        addLine(line, forNames) #remove this
-    else:
+        #check a line to see if it's a date, therefore a new day
+        newdate = re.compile('\s*[0-9]{1,2}-[0-9]{1,2}-[0-9]{1,2}\s*')
+        res = newdate.match(line)
+        if res != None: #date found
+            pass
+        else:
+            pass
         addLine(line, forNames)
+        guessNames(line)
+        line = f.readline()
 
 
 if __name__ == '__main__':
     #parser = argparse.ArgumentParser()
     #parser.parse_args()
-    main('/users/dirk/desktop/journal_test.txt')
+    #main('/users/dirk/desktop/journal_test.txt')
+    main('/Volumes/Disk Image/journal.rtf')
+    printHighest(5)
+
+    print 'guessedNamesSet: ',
+    print guessedNamesSet
+
+
